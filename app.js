cb6cf5e956149de1cbc09484d9535241691cf62b
kancherla.surendra@gmail.com <kancherla.surendra@gmail.com>
2017-02-12 22:44:41 +0530

    'init'

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'); //web application framework also called ExpressJS
+
+var app = express();
+
+/* http://localhost:3000 */
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+/* http://localhost:3000/login */
+app.post('/login', function(req, res) {
+	res.end("Login Successful.");
+});
+
+/* http://localhost:3000/queryProducts */
+app.get('/queryProducts', function(req, res) {
+
+	res.json([{name: 'iPhone7'}, {name: 'iPad mini'}]);
+});
+
+app.listen(3000, function() {
+	console.log("Server running on port 3000"); /* http://localhost:3000 */
+});
+
+console.log("a-quote-a-day project started");
